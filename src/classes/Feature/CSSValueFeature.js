7b9f91082d3f4e929b6661f01a1c241d56a0f97f
Lea Verou <lea@verou.me>
2025-09-09 17:55:33 +0300

    CSSValueFeature: dataTypes child

--- src/classes/Feature/CSSValueFeature.js
+++ src/classes/Feature/CSSValueFeature.js
@@ -16,15 +16,22 @@ export default class CSSValueFeature extends Feature {
 		},
 		args: {
 			single: 'arg',
-		}
+		},
+		dataTypes: {
+			single: 'dataType',
+		},
 	}
 
 	_createChildren () {
-		this.dataType = this.def.dataType;
-
 		// Use properties specified if available, or infer from type
 		// TODO this is run synchronously, so some specs may have have not had a chance to load yet
-		let properties = this.def.properties ?? CSSPropertyFeature.dataTypes[this.dataType]?.map(p => p.id);
+		let properties = this.def.properties;
+
+		let isLeaf = !(this.def.args || this.def.values || this.def.tests);
+		if (!properties && this.dataType && this.dataType in CSSPropertyFeature.dataTypes && isLeaf) {
+			properties = CSSPropertyFeature.dataTypes[this.dataType]?.map(p => p.id);
+		}
+
 		if (properties) {
 			if (!properties.processed) {
 				// Subset properties to remove unsupported ones before any children are created
@@ -51,27 +58,44 @@ export default class CSSValueFeature extends Feature {
 	get code () {
 		// TODO figure out when to show the property name too and return
 		// return `${this.property}: ${this.value}`;
-		if (this.def.fromParent === 'properties') {
-			return this.property;
+		switch (this.def.fromParent) {
+			case 'properties':
+				return this.property;
+			case 'dataTypes':
+				return `<${this.id}>`;
 		}
 
-		if (this.def.fromParent === 'args') {
-			return this.value;
-		}
-
-		return this.id;
+		return this.value;
 
 	}
 
-	get value () {
-		if (this.def.fromParent === 'args') {
-			let fn = this.parent.id;
-			return fn.replace(/\(\)$/, `(${this.id})`);
+	get dataType () {
+		if (this.def.fromParent === 'dataTypes') {
+			return this.id;
 		}
 
-		if (this.def.fromParent === 'properties') {
+		if (this.def.dataType) {
+			return this.def.dataType;
+		}
+
+		return;
+	}
+
+	set dataType (value) {
+		Object.defineProperty(this, 'dataType', { value });
+	}
+
+	get value () {
+		if (this.def.fromParent === 'properties' || this.def.fromParent === 'dataTypes') {
 			return this.parent.value;
 		}
+
+		if (this.def.fromParent === 'args') {
+			let fn = this.closest(f => f.id.endsWith('()'));
+			return fn.id.replace(/\(\)$/, `(${this.id})`);
+		}
+
+
 
 		if (this.args) {
 			// It's a CSS function.
